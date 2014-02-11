4b954ad1fc3a8bd2d25ff3189da417bef4a13367
Rolland I. Orgonas <rorgonas@gmail.com>
2014-02-11 12:44:24 -0500

    Configure Require.js and load initial dependencies by main.js bootstrap file

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Filename: main.js
+
+// Require.js allows us to configure shortcut alias
+require.config({
+  paths: {
+    jquery: 'libs/jquery/jquery-min',
+    underscore: 'libs/underscore/underscore-min',
+    backbone: 'libs/backbone/backbone-min'
+  }
+
+});
+
+require([
+
+  // Load our app module and pass it to our definition function
+  'app',
+], function(App){
+  // The "app" dependency is passed in as "App"
+  App.initialize();
+});
